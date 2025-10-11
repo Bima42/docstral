51d7c588707d10f55ce046de0e17ffeb7bc10797
Bima42 <tanpau42@gmail.com>
2025-10-11 21:49:27 +0200

    fix: change rate limit

--- server/core/rate_limiter.py
+++ server/core/rate_limiter.py
@@ -1,4 +1,4 @@
 import os
 
-AUTH_REQUESTS = int(os.getenv("RATE_LIMIT_AUTH_REQUESTS", 5))
+AUTH_REQUESTS = int(os.getenv("RATE_LIMIT_AUTH_REQUESTS", 20))
 AUTH_WINDOW = int(os.getenv("RATE_LIMIT_AUTH_WINDOW", 60))
